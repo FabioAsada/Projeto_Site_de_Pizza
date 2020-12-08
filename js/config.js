6ea67f939b8cb3c5485b625700eca4969df6b799
FabioAsada <fabioasada@hotmail.com>
2020-12-08 08:15:29 -0300

    commit site inteiro

---
+++ js/config.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+    //slider
+
+    $('.slider').slider();
+
+
+
+    
+    //Scrollspy
+
+    $('.scrollspy').scrollSpy({
+
+        scrollOffset: 80
+
+    });
+
+
+    //materialboxed
+
+    $('.materialboxed').materialbox();
+
+
+
+
+
+
+});
